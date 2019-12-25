26eb6619fd4c3157b24a731cd21a64e54d088bd8
BengBu-YueZhang <zhangyue_coder@163.com>
2019-12-26 01:39:55 +0800

    build

---
+++ build/precache-manifest.ee1db97657743a2c7784fdb331b771ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48218d661e61a8838a04d2ddf40722c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d21ccfecf0e2b689237",
+    "url": "/static/css/main.fc936a38.chunk.css"
+  },
+  {
+    "revision": "b1bfe911470c51382000",
+    "url": "/static/js/2.0fd61fd5.chunk.js"
+  },
+  {
+    "revision": "4d9d6b81dca703f872f395edfa5dc2bb",
+    "url": "/static/js/2.0fd61fd5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d21ccfecf0e2b689237",
+    "url": "/static/js/main.92cc7e6e.chunk.js"
+  },
+  {
+    "revision": "aeeed29373afa9eeabc3",
+    "url": "/static/js/runtime-main.8a7ea366.js"
+  },
+  {
+    "revision": "edcd12cfaf35882d0a04e790bfa4be85",
+    "url": "/static/media/lizhi.edcd12cf.jpg"
+  }
+]);
