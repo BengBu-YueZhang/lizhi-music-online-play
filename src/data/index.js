bd3b38ddfdd5b88395705e2bea9fc703ef033e19
BengBu-YueZhang <zhangyue_coder@163.com>
2019-12-26 02:29:24 +0800

    更新

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,12 @@
 const data = [
   {
-    src: "/heyJude.mp3",
-    title: "人民不需要自由",
+    src: "http://lizhimusic.bengbuzhangyue.xyz/%E5%85%B3%E4%BA%8E%E9%83%91%E5%B7%9E%E7%9A%84%E8%AE%B0%E5%BF%86.mp3",
+    title: "关于郑州的记忆",
+    artist: "李志"
+  },
+  {
+    src: "http://lizhimusic.bengbuzhangyue.xyz/%E5%B9%BF%E5%9C%BA.mp3",
+    title: "广场",
     artist: "李志"
   }
 ];
